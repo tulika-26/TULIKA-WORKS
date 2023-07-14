e4182d8b6ec850e7e9518c73be0bddeb53f62638
tulika-26 <119873819+tulika-26@users.noreply.github.com>
2023-07-14 23:17:03 +0530

    Add files via upload

---
+++ ytdownloader.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from pytube import YouTube
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+
+link = input("Enter the YouTube video URL: ")
+Download(link)
